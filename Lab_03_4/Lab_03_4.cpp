dce43738a7b3fcda14708ef6ddb8afdd5744d983
Glit <vpad.lazar.04@gmail.com>
2022-10-14 16:06:45 +0300

    LR-03.4

---
+++ Lab_03_4/Lab_03_4.cpp
@@ -0,0 +1,32 @@
+// Lab_03_4.cpp
+// Лазар Владислав
+// Лабораторна робота No 3.4
+// Розгалуження, задане плоскою фігурою.
+// Варіант 11
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	double x;
+	double y;
+	double R;
+
+	cout << "x = "; cin >> x;
+	cout << "y = "; cin >> y;
+	cout << "R = "; cin >> R;
+	
+	if (y <= 0 && y >= -x - R || y <= sqrt(R*R - x*x) && y >= 0)
+	{
+		cout << "yes" << endl;
+	}
+	else
+	{
+		cout << "no" << endl;
+	}
+
+	return 0;
+}
