983c1a3c9e423327eb26e4766defc83191f30631
Glit <vpad.lazar.04@gmail.com>
2022-10-22 20:13:30 +0300

    fix

--- Lab_03_4/Lab_03_4.cpp
+++ Lab_03_4/Lab_03_4.cpp
@@ -5,15 +5,12 @@
 // Варіант 11
 
 #include <iostream>
-#include <cmath>
 
 using namespace std;
 
 int main()
 {
-	double x;
-	double y;
-	double R;
+	double x, y, R;
 
 	cout << "x = "; cin >> x;
 	cout << "y = "; cin >> y;
